d1fb308377e114243ef5fea719943b571854aa0f
pcummuskey <pcummuskey@bravurasolutions.com>
2015-11-02 10:58:15 +0000

    Added better option handling and pathing.

--- src/main/plugin.js
+++ src/main/plugin.js
@@ -4,6 +4,7 @@
 var gutil = require('gulp-util');
 var through2 = require('through2');
 var browserify = require('browserify');
+var paths = require('path');
 
 var PluginError = gutil.PluginError;
 
@@ -13,16 +14,47 @@ var PluginError = gutil.PluginError;
  */
 module.exports = function browserificator(options) {
 
+	var config = {
+
+		debug: options
+			&& options.debug,
+
+		basedir: options
+			&& options.basedir,
+
+		paths: options
+			&& options.paths,
+
+		preBundle: options
+			&& options.preBundle,
+
+		push: options
+			&& options.push
+
+	};
+
 	return through2.obj(function(file, enc, done) {
-		
+		var self = this;
+
 		if (file.isNull()) {
 			return done(null, file);
 		}
-		
-		var bundle = browserify(file, options)
 
-		if (typeof(options.preBundle) === 'function') {
-			bundle = options.preBundle(bundle);
+		if (!config.basedir) {
+			//config.basedir = file.base;
+		}
+
+		if (!config.paths) {
+			config.paths = [
+				file.base,
+				file.cwd
+			]
+		}
+
+		var bundle = browserify(file, config);
+
+		if (typeof(config.preBundle) === 'function') {
+			bundle = config.preBundle(bundle);
 			if (!bundle) {
 				return done(new PluginError('gulp-recipe-browserify',
 					'You forgot to return the bundle'));
@@ -32,10 +64,25 @@ module.exports = function browserificator(options) {
 		return bundle.bundle(function(error, result) {
 
 			if (error) {
+
+				if (config.debug) {
+					gutil.log('gulp-recipe-browserify', 'Failed to browserify ' + file.path);
+				}
+
 				return done(error);
+
 			}
-			
+
 			file.contents = result;
+
+			if (config.push) {
+				self.push(file);
+			}
+
+			if (config.debug) {
+				gutil.log('gulp-recipe-browserify', 'Successfully browserified ' + file.path);
+			}
+
 			return done(null, file);
 
 		});
